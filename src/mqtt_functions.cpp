1a3f3aae1ee3222a33fdad5427ac148948436c29
Patrik Hermansson <hermansson.patrik@gmail.com>
2022-08-17 15:19:59 +0200

    Async Mqtt not working well

--- src/mqtt_functions.cpp
+++ src/mqtt_functions.cpp
@@ -1,33 +1,50 @@
 #include <mqtt_functions.h>
 
-AsyncMqttClient mqtt_client;
+//WiFiClient espClient;
+//PubSubClient pubsub(espClient);
+  AsyncMqttClient mqttClient;
 Ticker mqttReconnectTimer;
 
-void mqtt_setup()
+void mqtt_init()
 {
-  mqtt_client.onConnect(onMqttConnect);
-  mqtt_client.onDisconnect(onMqttDisconnect);
-  mqtt_client.onPublish(onMqttPublish);
-  mqtt_client.setServer(MQTT_SERVER, MQTT_PORT);
+
+//  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
+//  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+  Serial.println("MQTT init");
+
+
 }
 
-void mqtt_publish()
-{
-     uint16_t packetIdPub1 = mqtt_client.publish(MQTT_PUB_TEST, 1, true, "Test");
-    Serial.printf("Publishing on topic %s at QoS 1, packetId: %i", MQTT_PUB_TEST, packetIdPub1);
-    Serial.printf("Message: %s \n", "Test");
-}
-
-
-void connectToMqtt() {  // TODO: Rename this!
+void connectToMqtt() {
   Serial.println("Connecting to MQTT...");
-  mqtt_client.connect();
+  mqttClient.connect();
+}
+
+void publishMqtt(char *data) {
+  uint16_t packetIdPub = mqttClient.publish("test/lol", 0, true, "data");
+  Serial.print("Publishing at QoS 0, packetId: ");
+  Serial.println(packetIdPub);
 }
 
 void onMqttConnect(bool sessionPresent) {
   Serial.println("Connected to MQTT.");
   Serial.print("Session present: ");
   Serial.println(sessionPresent);
+  /*uint16_t packetIdSub = mqttClient.subscribe("test/lol", 2);
+  Serial.print("Subscribing at QoS 2, packetId: ");
+  Serial.println(packetIdSub);
+  mqttClient.publish("test/lol", 0, true, "test 1");
+  Serial.println("Publishing at QoS 0");
+  uint16_t packetIdPub1 = mqttClient.publish("test/lol", 1, true, "test 2");
+  Serial.print("Publishing at QoS 1, packetId: ");
+  Serial.println(packetIdPub1);
+  */
+  uint16_t packetIdPub2 = mqttClient.publish("test/lol", 2, true, "test 3");
+  Serial.print("Publishing at QoS 2, packetId: ");
+  Serial.println(packetIdPub2);
+    char data[] = "Hello World!";
+
+  publishMqtt(data);
 }
 
 void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
@@ -38,36 +55,71 @@ void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
   }
 }
 
+void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  Serial.println("Publish received.");
+  Serial.print("  topic: ");
+  Serial.println(topic);
+  Serial.print("  qos: ");
+  Serial.println(properties.qos);
+  Serial.print("  dup: ");
+  Serial.println(properties.dup);
+  Serial.print("  retain: ");
+  Serial.println(properties.retain);
+  Serial.print("  len: ");
+  Serial.println(len);
+  Serial.print("  index: ");
+  Serial.println(index);
+  Serial.print("  total: ");
+  Serial.println(total);
+}
+
 void onMqttPublish(uint16_t packetId) {
-  Serial.print("Publish acknowledged.");
+  Serial.println("Publish acknowledged.");
   Serial.print("  packetId: ");
   Serial.println(packetId);
 }
 
 
-// Old function remove!?
-void mqtt_reconnect(PubSubClient pubsub, WiFiClient espClient, uint8_t debugflag) {
+/*
+//void mqtt_reconnect(PubSubClient pubsub, WiFiClient espClient, uint8_t debugflag) {
+  void mqtt_reconnect(WiFiClient esp_client) {
   pubsub.setServer(MQTT_SERVER, MQTT_PORT);
   pubsub.setClient(espClient);
 
   while (!pubsub.connected()) {
     Serial.print("Attempting MQTT connection...");
+     pubsub.setKeepAlive( 90 ); 
     // Attempt to connect
     //if (pubsub.connect(APPNAME, MQTT_USERNAME, MQTT_PASSWORD)) {
       if (pubsub.connect(APPNAME)) {
-    #ifdef DEBUG
+        #ifdef DEBUG
       
-        String temp = "Connected to Mqtt broker as " + String(APPNAME);
-        Serial.println(temp);
-        //StaticJsonBuffer<150> jsonBuffer;
-        //JsonObject& root = jsonBuffer.createObject();
-        char msg[150];
-        sprintf(msg, "Hello World!");
-        //root["status"] = temp;
-        //root.printTo((char*)msg, root.measureLength() + 1);
-        pubsub.publish(MQTT_DEBUG_TOPIC, msg);
-    #endif  
-      
+          String temp = "Connected to Mqtt broker as " + String(APPNAME);
+          Serial.println(temp);
+          //StaticJsonBuffer<150> jsonBuffer;
+          //JsonObject& root = jsonBuffer.createObject();
+          char msg[150];
+          sprintf(msg, "Hello World!");
+          //root["status"] = temp;
+          //root.printTo((char*)msg, root.measureLength() + 1);
+          pubsub.publish(MQTT_DEBUG_TOPIC, msg);
+          Serial.println("Published test message");
+        #endif  
+
     } else {
       Serial.print("Mqtt connection failed, rc=");
       Serial.print(pubsub.state());
@@ -77,3 +129,4 @@ void mqtt_reconnect(PubSubClient pubsub, WiFiClient espClient, uint8_t debugflag
     }
   }
 }
+*/
